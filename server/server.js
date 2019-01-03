9cefa1e2db899ad8eb63b7b992316dba8ae7ee09
t.thiticate <t.thiticate@unesco.org>
2019-01-03 16:06:38 +0700

    Init commit

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const path = require('path');
-
+const scoketIO = require('socket.io');
+const http = require('http');
 var publicPath = path.join(__dirname , '../public' );
 
 
 
 var app = express();
 var port =   process.env.port || 3000;
+var server = http.createServer(app);
+
+
 
 console.log(__dirname + '/public');
 console.log(publicPath);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Started up at port ${port}`);
   });
   
