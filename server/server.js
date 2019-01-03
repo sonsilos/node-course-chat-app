21cadc744f07ce14c70ebbd74ce346fffb65482d
t.thiticate <t.thiticate@unesco.org>
2019-01-03 17:10:41 +0700

    Add push noti

--- server/server.js
+++ server/server.js
@@ -1,23 +1,50 @@
-const express = require('express');
 const path = require('path');
-const scoketIO = require('socket.io');
 const http = require('http');
-var publicPath = path.join(__dirname , '../public' );
+const express = require('express');
+const socketIO = require('socket.io');
 
-
-
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
-var port =   process.env.PORT || 3000;
 var server = http.createServer(app);
-
-
-
-console.log(__dirname + '/public');
-console.log(publicPath);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-server.listen(port, () => {
-    console.log(`Started up at port ${port}`);
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
   });
-  
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
