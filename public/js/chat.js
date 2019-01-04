6ecf0a143d3e17f5ff51cadab060a223d0ed15d2
t.thiticate <t.thiticate@unesco.org>
2019-01-04 11:52:56 +0700

    Comolete chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,24 +11,36 @@ function scrollToBottom () {
   var newMessageHeight = newMessage.innerHeight();
   var lastMessageHeight = newMessage.prev().innerHeight();
 
-
-
-  console.log('clientHeight : ' + clientHeight);
-  console.log('scrollTop : ' + scrollTop);
-  console.log('scrollHeight : ' + scrollHeight);
-  console.log('newMessageHeight : ' + newMessageHeight);
-  console.log('lastMessageHeight : ' + lastMessageHeight);
-  if (clientHeight + scrollTop + newMessageHeight >= scrollHeight) {
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
     messages.scrollTop(scrollHeight);
   }
 }
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
@@ -63,7 +75,6 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
